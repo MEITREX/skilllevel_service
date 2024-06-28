1408f2aa9489d5a17c9f931714505dd1370da396
PaulBredl <paulbredl@gmx.de>
2024-06-28 20:51:06 +0200

    rename package to meitrex

---
+++ src/main/java/de/unistuttgart/iste/meitrex/skilllevel_service/SkillLevelServiceApplication.java
@@ -0,0 +1,16 @@
+package de.unistuttgart.iste.meitrex.skilllevel_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the entry point of the application.
+ */
+@SpringBootApplication
+public class SkillLevelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillLevelServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/de/unistuttgart/iste/meitrex/skilllevel_service/persistence/repository/AllSkillLevelsRepository.java
@@ -0,0 +1,15 @@
+package de.unistuttgart.iste.meitrex.skilllevel_service.persistence.repository;
+
+import de.unistuttgart.iste.meitrex.skilllevel_service.persistence.entity.AllSkillLevelsEntity;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@Profile("!test")
+public interface AllSkillLevelsRepository extends JpaRepository<AllSkillLevelsEntity, AllSkillLevelsEntity.PrimaryKey> {
+    List<AllSkillLevelsEntity> findByIdChapterId(UUID chapterId);
+}
